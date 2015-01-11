c5872583ce27c4438f1d50a49413c18a94bc145a
h2rashee <harrisrasheed@outlook.com>
2015-01-11 05:06:27 +0400

    2^n solution to find lowest amount of energy needed

--- delivery.py
+++ delivery.py
@@ -2,14 +2,54 @@ import sys
 
 jetStreams = []
 mileCost = 0
+endPoint = 0
 
 
 def findShortestPath(curPos, curPath):
-    pass
+    if curPos < endPoint:
+        regularFlight = getNextJetStream(curPos)
+
+        fly = ((regularFlight[0]-curPos) * mileCost) + findShortestPath(regularFlight[0], curPath)
+
+        if regularFlight:
+            jet = min([st[2] + findShortestPath(st[1],
+                curPath.append(st))
+                for st in getCurrentJetStreams(curPos)], key = lambda x:x[1])
+        else:
+            return fly
+
+        return fly if fly < jet else jet
+    return 0
 
 
-def getJetStreamCost(index):
-    return jetStreams[index][2]
+def getNextJetStream(curPos):
+    '''Get the next jetstream that the swallow is sitting on'''
+    for stream in jetStreams:
+        if stream[0] > curPos:
+            return stream
+    return None
+
+
+def getCurrentJetStreams(curPos):
+    '''Given the current position, return the distance to the next jetstream'''
+    lookingForSame = false
+    streams = []
+
+    for stream in jetStreams:
+        if stream[0] >= curPos and not lookingForSame:
+            lookingForSame = true
+            streams.append(stream)
+        elif lookingForSame and stream[0] == streams[0][0]:
+            streams.append(stream)
+        elif lookingForSame and stream[0] != streams[0][0]:
+            return streams
+    return streams
+
+
+def findEndPoint():
+    '''Find the end point that the swallow needs to travel'''
+    '''based on the ending point of the farthest jetstream'''
+    return max(jetStreams, key=lambda x:x[1])[1]
 
 
 if __name__ == "__main__":
@@ -25,4 +65,5 @@ if __name__ == "__main__":
     jetStreams = sorted(jetStreams, key=lambda x:x[0])
 
     pos = 0
-    findShortestPath(pos, [])
+    endPoint = findEndPoint()
+    print findShortestPath(pos, [])
