2e45dd42828b18f4a36c223b4a08ed7594ce4987
h2rashee <harrisrasheed@outlook.com>
2015-02-06 03:37:50 +0400

    Dynamic programming implementation of finding minimal cost jetstream combo for the swallow

--- delivery.py
+++ delivery.py
@@ -1,57 +1,99 @@
-import sys
-
+# Variables initialised on start-up
 jetStreams = []
 mileCost = 0
-endPoint = 0
+
+# Stores the dynamic programming results of the minimum cost of getting to the
+# end point of that jetstream (matches jetstream index number as above)
+endOfJsCost = []
+
+# Stores whether the matching jetstream (index) had it's minimal cost derived
+# through a previous jetstream or none
+prevStreamUsed = []
 
 
-def findShortestPath(curPos, curPath):
-    if curPos < endPoint:
-        regularFlight = getNextJetStream(curPos)
+def findMinCostPath():
+    '''Calculate the minimum cost of flying from the start to end'''
+    '''for a swallow making efficient use of the jetstreams'''
 
-        fly = ((regularFlight[0]-curPos) * mileCost) + findShortestPath(regularFlight[0], curPath)
+    for i in range(0, len(jetStreams)):
+        # Cost of walking to all the way to the current stream and then using it
+        walkAllTheWay = getStretchCost(0, jetStreams[i][0]) + jetStreams[i][2]
+        minCost = walkAllTheWay
+        # Keep track that there was no previous stream we used here
+        prevUsedStream = -1
 
-        if regularFlight:
-            jet = min([st[2] + findShortestPath(st[1],
-                curPath.append(st))
-                for st in getCurrentJetStreams(curPos)], key = lambda x:x[1])
-        else:
-            return fly
+        # Cost of using a previous stream and the current one
+        for j in range(0, i):
+            # Only use the stream if it's end is before the current's start
+            if jetStreams[j][1] < jetStreams[i][0]:
+                curCost = usePreviousStream(i, j)
+            # All the streams from hereinafter,
+            else:
+                # have their end after the current's start so we stop
+                break
 
-        return fly if fly < jet else jet
-    return 0
+            if curCost < minCost:
+                minCost = curCost
+                # Keep track of the chain of jetstreams we used
+                prevUsedStream = j
 
+        # Cost of using a previous stream but not the current one
+        for j in range(0, i):
+            curCost = endOfJsCost[j] + \
+                        getStretchCost(jetStreams[j][1], jetStreams[i][1])
 
-def getNextJetStream(curPos):
-    '''Get the next jetstream that the swallow is sitting on'''
-    for stream in jetStreams:
-        if stream[0] > curPos:
-            return stream
-    return None
+            if curCost < minCost:
+                minCost = curCost
+                # Keep track of the chain of jetstreams we used
+                prevUsedStream = j
 
-
-def getCurrentJetStreams(curPos):
-    '''Given the current position, return the distance to the next jetstream'''
-    lookingForSame = false
-    streams = []
-
-    for stream in jetStreams:
-        if stream[0] >= curPos and not lookingForSame:
-            lookingForSame = true
-            streams.append(stream)
-        elif lookingForSame and stream[0] == streams[0][0]:
-            streams.append(stream)
-        elif lookingForSame and stream[0] != streams[0][0]:
-            return streams
-    return streams
-
-
-def findEndPoint():
-    '''Find the end point that the swallow needs to travel'''
-    '''based on the ending point of the farthest jetstream'''
-    return max(jetStreams, key=lambda x:x[1])[1]
+        # Add the minimum cost to our memoisation storing minimum costs for JSes
+        endOfJsCost.append(minCost)
+        prevStreamUsed.append(prevUsedStream)
+    return endOfJsCost[-1]
 
 
+def usePreviousStream(cur, prev):
+    '''Calculate the cost of using a previous stream (given the index),'''
+    '''walking to the current stream and then using the current stream'''
+
+    #1) Best way to get to the previous stream has already been calculated.
+    #2) The cost of walking from the previous jetstream to the current one.
+    #3) The cost of using the current jetstream
+
+    return endOfJsCost[prev] + \
+            getStretchCost(jetStreams[prev][1], jetStreams[cur][0]) + \
+            jetStreams[cur][2]
+
+
+def getStretchCost(start, end):
+    '''Calculate the energy cost of travelling without a jetstream between'''
+    '''a given start and end point'''
+
+    return (end-start)*mileCost
+
+
+def getOptimalJetStreams():
+    '''Find the chain of jetstreams used for the optimal path based on the'''
+    '''bread crumb trail we left behind'''
+
+    i = len(prevStreamUsed) - 1         # We start at the last jetstream
+    usingJetStreams = []
+
+    # For each jetstream that used a previous jetstream
+    while i != -1:
+        # Extract just the start and end points
+        usingJetStreams.append([jetStreams[i][0], jetStreams[i][1]])
+        # and look at the next jetstream in the chain
+        i = prevStreamUsed[i]
+
+    # We remember to flip the list since we built it back-to-front
+    usingJetStreams.reverse()
+    return usingJetStreams
+
+
+
+# MAIN/DRIVER
 if __name__ == "__main__":
     try:
         with open('flight_paths.txt') as f:
@@ -62,8 +104,8 @@ if __name__ == "__main__":
         print "Unexpected error while reading file"
         sys.exit(1)
 
-    jetStreams = sorted(jetStreams, key=lambda x:x[0])
+    # Sort the jetstreams by their ending point
+    jetStreams = sorted(jetStreams, key=lambda x:x[1])
 
-    pos = 0
-    endPoint = findEndPoint()
-    print findShortestPath(pos, [])
+    print findMinCostPath()
+    print getOptimalJetStreams()
